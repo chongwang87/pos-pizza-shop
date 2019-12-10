f4f4665949846cc5a6f76ae96b4c09e3b5c33386
Koo Chong Wang <chongwang87@gmail.com>
2019-12-11 00:10:12 +0800

    add popup modal to New Order btn

--- imports/ui/_component/Dashboard/index.jsx
+++ imports/ui/_component/Dashboard/index.jsx
@@ -5,77 +5,107 @@ import Box from '@material-ui/core/Box'
 import Button from '@material-ui/core/Button'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
 import Grid from '@material-ui/core/Grid'
+import Modal from '@material-ui/core/Modal'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 
 import PlusIcon from '@material-ui/icons/Add'
-import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack'
 
 const useStyles = makeStyles(theme => ({
-  root : {
-    color: theme.palette.primary
-  },
-  title: {
-    margin : theme.spacing(2, 0)
-  },
-  inline : {
-    display : 'inline-block'
-  },
-  button: {},
+	root: {
+		color: theme.palette.primary
+	},
+	title: {
+		margin: theme.spacing(2, 0)
+	},
+	inline: {
+		display: 'inline-block'
+	},
+	button: {},
+	modal : {
+		position: 'absolute',
+		top: theme.spacing(2),
+		left: '50%',
+		transform: 'translate(-50%, 0)',
+		width: '80%',
+		maxWidth: 800,
+		backgroundColor: theme.palette.background.paper,
+		boxShadow: theme.shadows[5],
+		padding: theme.spacing(2, 4, 3),
+	}
 }))
 
 export default function Dashboard() {
-  const classes = useStyles(),
-    history = useHistory()
+	const classes = useStyles(),
+		[open, setOpen] = React.useState(false)
 
-  return (
-    <Grid container spacing={ 2 }>
-      <Grid item xs={ 12 }>
-        <Typography
-          className={ classes.title }
-          variant="h2"
-        >
-          Welcome to { ' ' }
-          <Box color="primary.main" fontWeight={ 600 } className={ classes.inline }>Popular Pizza</Box>!
-        </Typography>
-        <ButtonGroup variant="contained">
-          { useRouteMatch("/admin") ?
-            <Button
-              variant="contained"
-              className={ classes.button }
-              startIcon={ <ArrowBackIcon /> }
-              size="small"
-              component={ Link }
-              to="/"
-            >
-              Back
-            </Button>
-            :
-            <>
-              <Button
-                variant="contained"
-                color="primary"
-                className={ classes.button }
-                startIcon={ <PlusIcon /> }
-                size="large"
-              >
-                New Order
-              </Button>
-                  <Button
-                    variant="contained"
-                    className={ classes.button }
-                    startIcon={ <SupervisorAccountIcon /> }
-                    size="large"
-                    component={ Link }
-                    to="/admin"
-                  >
-                    Admin
-              </Button>
-            </>
-          }
-        </ButtonGroup>
-      </Grid>
-    </Grid>
-  )
+	const handleOpen = () => {
+		setOpen(true)
+	}
+
+	const handleClose = () => {
+		setOpen(false)
+	}
+
+	return (
+		<Grid container spacing={ 2 }>
+			<Grid item xs={ 12 }>
+				<Typography
+					className={ classes.title }
+					variant="h2"
+				>
+					Welcome to { ' ' }
+					<Box color="primary.main" fontWeight={ 600 } className={ classes.inline }>Popular Pizza</Box>!
+        		</Typography>
+				{ useRouteMatch("/admin") ?
+					<Button
+						variant="contained"
+						className={ classes.button }
+						startIcon={ <ArrowBackIcon /> }
+						size="small"
+						component={ Link }
+						to="/"
+					>
+						Back
+          		</Button>
+					:
+					<>
+						<ButtonGroup variant="contained">
+							<Button
+								variant="contained"
+								color="primary"
+								className={ classes.button }
+								startIcon={ <PlusIcon /> }
+								size="large"
+								onClick={ handleOpen }
+							>
+								New Order
+							</Button>
+							<Button
+								variant="contained"
+								className={ classes.button }
+								startIcon={ <SupervisorAccountIcon /> }
+								size="large"
+								component={ Link }
+								to="/admin"
+							>
+								Admin
+							</Button>
+						</ButtonGroup>
+						<Modal
+							open={ open }
+							onClose={ handleClose }
+						>
+							<div className={ classes.modal }>
+								<h2>Text in a modal</h2>
+								<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+							</div>
+						</Modal>
+					</>
+				}
+			</Grid>
+		</Grid>
+	)
 }
