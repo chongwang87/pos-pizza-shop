9ef384989c9a8933b9bff955c44928d740253874
Koo Chong Wang <chongwang87@gmail.com>
2019-12-10 11:27:54 +0800

    add in default MUI theme & override some default theme

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,19 +2,30 @@ import React from 'react';
 
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Container from '@material-ui/core/Container'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 
-import Hello from './Hello.jsx';
+import blue from '@material-ui/core/colors/blue';
+import red from '@material-ui/core/colors/red'
+
+import Dashboard from './Dashboard';
 import Order from './Order';
 
-const App = () => (
-  <>
-    <CssBaseline />
-    <Container>
-      <h1>Welcome to Popular Pizza!</h1>
-      <Hello />
-      <Order />
-    </Container>
-  </>
-);
-
-export default App;
+export default function App() {
+  const theme = createMuiTheme({
+    palette: {
+      primary: blue,
+      secondary: red,
+    }
+  })
+  return (
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={ theme }>
+        <Container>
+          <Dashboard />
+          <Order />
+        </Container>
+      </ThemeProvider>
+    </>
+  )
+}
