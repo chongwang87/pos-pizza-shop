374ef165532438f775c5ff28d4be6385fa1c55ef
Koo Chong Wang <chongwang87@gmail.com>
2019-12-10 23:40:26 +0800

    add ensureIndex to db

--- imports/api/menus/index.js
+++ imports/api/menus/index.js
@@ -1,3 +1,11 @@
-import { Mongo } from 'meteor/mongo';
+import { Mongo } from 'meteor/mongo'
+import { Meteor } from 'meteor/meteor'
 
-export default Menus = new Mongo.Collection('menus');
+export default Menus = new Mongo.Collection('menus')
+
+if (Meteor.isServer){
+	Meteor.startup(() => {
+		Menus._ensureIndex({ type: 1 })
+		Menus._ensureIndex({ name: 1 }, { unique: true })
+	})
+}
