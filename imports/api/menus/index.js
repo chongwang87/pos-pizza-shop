644f8a528dfbb753e752204d04aa4f913ed10789
Koo Chong Wang <chongwang87@gmail.com>
2019-12-11 00:09:43 +0800

    fix unique index

--- imports/api/menus/index.js
+++ imports/api/menus/index.js
@@ -6,6 +6,6 @@ export default Menus = new Mongo.Collection('menus')
 if (Meteor.isServer){
 	Meteor.startup(() => {
 		Menus._ensureIndex({ type: 1 })
-		Menus._ensureIndex({ name: 1 }, { unique: true })
+		Menus._ensureIndex({ type: 1, name: 1 }, { unique: true })
 	})
 }
