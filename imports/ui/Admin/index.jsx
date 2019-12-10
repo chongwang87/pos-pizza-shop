753bd18ba6ca24bfd37530ad4947c1d9dde657dd
Koo Chong Wang <chongwang87@gmail.com>
2019-12-10 23:41:06 +0800

    create menus table & CRUD process

---
+++ imports/ui/Admin/index.jsx
@@ -0,0 +1,117 @@
+import React, { Component } from 'react'
+import MaterialTable from 'material-table'
+import { withTracker } from 'meteor/react-meteor-data'
+
+import AppBar from '@material-ui/core/AppBar'
+import Box from '@material-ui/core/Box'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+
+import Menus from '../../api/menus'
+
+import Dashboard from '../_component/Dashboard'
+
+const useStyles = makeStyles(theme => ({
+	introduction : {
+		margin : theme.spacing(2, 0)
+	},
+	tabs: {
+		backgroundColor: theme.palette.background.paper,
+		margin : theme.spacing(2, 0)
+	},
+}))
+
+function Admin(props) {
+	const classes = useStyles(),
+		[value, setValue] = React.useState('flavour'),
+		[state, setState] = React.useState({
+			columns: [
+				{ title: 'Name', field: 'name' },
+				{ title: 'Value', field: 'price', type: 'numeric' }
+			],
+			data: props.menus(value),
+		})
+
+	const handleChange = (event, newValue) => {
+			setValue(newValue)
+			setState(prevState => {
+				return { ...prevState, data: props.menus(newValue) }
+			})
+		}
+
+	return (
+		<>
+			<Dashboard />
+			<Typography className={ classes.introduction } variant="body1">
+				<Box>Customise your menus</Box>
+				<Box>Pizza price = Flavor X (Size + Crust + Addons..)</Box>
+			</Typography>	
+			<div className={ classes.tabs }>
+				<AppBar position="static">
+					<Tabs
+						value={ value }
+						onChange={ handleChange }
+					>
+						<Tab label="Flavour" value="flavour" />
+						<Tab label="Size" value="size" />
+						<Tab label="Crust" value="crust" />
+						<Tab label="Addon" value="addon" />
+					</Tabs>
+				</AppBar>
+				<MaterialTable
+					title={ value.toLocaleString() }
+					columns={ state.columns }
+					data={ state.data }
+					options={
+						{
+							pageSize: 20,
+							actionsColumnIndex: 2
+						}
+					}
+					editable={ {
+						onRowAdd: newData =>
+							new Promise(resolve => {
+								newData.type = value
+								resolve()
+								Menus.insert({
+									...newData,
+									createdAt: new Date()
+								})
+								setState(prevState => {
+									return { ...prevState, data: props.menus(value) }
+								})
+							}),
+						onRowUpdate: (newData, oldData) =>
+							new Promise(resolve => {
+								resolve()
+								if (oldData) {
+									Menus.update({ _id: oldData._id }, { $set: newData })
+									setState(prevState => {
+										return { ...prevState, data: props.menus(value) }
+									})
+								}
+							}),
+						onRowDelete: oldData =>
+							new Promise(resolve => {
+								resolve()
+								Menus.remove({ _id: oldData._id })
+								setState(prevState => {
+									return { ...prevState, data: props.menus(value) }
+								})
+							}),
+					} }
+				/>
+			</div>
+		</>
+	)
+}
+
+export default withTracker(() => {
+	return {
+		menus: (type = 'flavor') => {
+			return Menus.find({ type : type }).fetch()
+		}
+	}
+})(Admin)
