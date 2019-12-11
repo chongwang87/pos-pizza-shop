3572cdb7ebb607ca6aa542c5798882ae0c1dd3b1
Koo Chong Wang <chongwang87@gmail.com>
2019-12-12 05:06:39 +0800

    clean codes

--- imports/ui/_component/OrderHistory/index.jsx
+++ imports/ui/_component/OrderHistory/index.jsx
@@ -16,85 +16,83 @@ import Orders from '../../../api/orders'
 
 const columns = [
 	{
-		id: 'flavour',
-		label: 'Flavour',
-		format: v => v.toLocaleString()
+		id : 'flavour',
+		label : 'Flavour',
+		format : v => v.toLocaleString()
 	}, {
-		id: 'size',
-		label: 'Size',
-		align: 'right'
+		id : 'size',
+		label : 'Size',
+		align : 'right'
 	}, {
-		id: 'crust',
-		label: 'Crust',
-		format: v => v.toLocaleString(),
+		id : 'crust',
+		label : 'Crust',
+		format : v => v.toLocaleString()
 	}, {
-		id: 'additional',
-		label: 'Additional',
-		format: v => v.map((e, i) => {
+		id : 'additional',
+		label : 'Additional',
+		format : v => v.map((e, i) => {
 			return `${ (i > 0) ? ',' : '' } ${ e.toLocaleString() }`
-		}),
+		})
 	}, {
-		id: 'price',
-		label: 'Price',
-		align: 'right',
-		format: v => v.toFixed(2),
-	},
+		id : 'price',
+		label : 'Price',
+		align : 'right',
+		format : v => v.toFixed(2)
+	}
 ]
 
 function createData(id, flavour, size, crust, additional, price) {
-	return { id, flavour, size, crust, additional, price }
+	return {
+		id, flavour, size, crust, additional, price
+	}
 }
 
 const rows = [
-	createData(1, 'Hawaiian', 14, 'Pan', ['mushroom','cheese'], 18.90),
-	createData(2, 'Chicken Supreme', 14, 'Pan', ['mushroom'], 18.90),
-	createData(3, 'Super Supreme', 7, 'Pan', ['mushroom'], 18.90),
-	createData(4, 'Pepperoni', 14, 'Pan', ['mushroom'], 18.90),
-	createData(5, 'Veggie Lover\'s', 12, 'Pan', ['mushroom'], 18.90),
-	createData(6, 'Curry Chicken', 7, 'Pan', ['mushroom'], 18.90),
-	createData(7, 'BBQ Chunky Chic', 12, 'Pan', ['mushroom'], 18.90),
-	createData(8, 'Chic Ham \'N\' Shroom',7, 'Pan', ['mushroom'], 18.90),
-	createData(9, 'Cheese \'N\' Chic', 12, 'Pan', ['mushroom'], 18.90),
-	createData(10, 'Simply Cheese', 12, 'Pan', ['mushroom'], 18.90),
-	createData(11, 'Ocean Delight', 7, 'Pan', ['mushroom'], 18.90),
-	createData(12, 'Very Beefy', 12, 'Pan', ['mushroom'], 18.90),
-	createData(13, 'Wild About Mushrooms', 12, 'Pan', ['mushroom'], 18.90),
-	createData(14, 'BBQ Chicken', 7, 'Pan', ['mushroom'], 18.90),
-	createData(15, 'The Four Cheese', 12, 'Pan', ['mushroom'], 18.90),
-	createData(16, 'Seafood Deluxe', 7, 'Pan', ['mushroom'], 18.90),
-	createData(17, 'Hawaiian Supreme', 12, 'Pan', ['mushroom'], 18.90),
-]
+		createData(1, 'Hawaiian', 14, 'Pan', ['mushroom', 'cheese'], 18.90),
+		createData(2, 'Chicken Supreme', 14, 'Pan', ['mushroom'], 18.90),
+		createData(3, 'Super Supreme', 7, 'Pan', ['mushroom'], 18.90),
+		createData(4, 'Pepperoni', 14, 'Pan', ['mushroom'], 18.90),
+		createData(5, 'Veggie Lover\'s', 12, 'Pan', ['mushroom'], 18.90),
+		createData(6, 'Curry Chicken', 7, 'Pan', ['mushroom'], 18.90),
+		createData(7, 'BBQ Chunky Chic', 12, 'Pan', ['mushroom'], 18.90),
+		createData(8, 'Chic Ham \'N\' Shroom', 7, 'Pan', ['mushroom'], 18.90),
+		createData(9, 'Cheese \'N\' Chic', 12, 'Pan', ['mushroom'], 18.90),
+		createData(10, 'Simply Cheese', 12, 'Pan', ['mushroom'], 18.90),
+		createData(11, 'Ocean Delight', 7, 'Pan', ['mushroom'], 18.90),
+		createData(12, 'Very Beefy', 12, 'Pan', ['mushroom'], 18.90),
+		createData(13, 'Wild About Mushrooms', 12, 'Pan', ['mushroom'], 18.90),
+		createData(14, 'BBQ Chicken', 7, 'Pan', ['mushroom'], 18.90),
+		createData(15, 'The Four Cheese', 12, 'Pan', ['mushroom'], 18.90),
+		createData(16, 'Seafood Deluxe', 7, 'Pan', ['mushroom'], 18.90),
+		createData(17, 'Hawaiian Supreme', 12, 'Pan', ['mushroom'], 18.90)
+	],
 
-const useStyles = makeStyles(theme => ({
-	root: {
-		width: '100%',
-	},
-	title: {
-		flex: '1 1 100%',
-		margin: theme.spacing(2, 0)
-	},
-	tableWrapper: {
-		maxHeight: 600,
-		overflow: 'auto',
-	},
-	tableCell:{
-		whiteSpace : 'nowrap'
-	}
-}))
+	useStyles = makeStyles(theme => ({
+		root : { width : '100%' },
+		title : {
+			flex : '1 1 100%',
+			margin : theme.spacing(2, 0)
+		},
+		tableWrapper : {
+			maxHeight : 600,
+			overflow : 'auto'
+		},
+		tableCell : { whiteSpace : 'nowrap' }
+	}))
 
 function OrderHistory() {
-	const classes = useStyles()
-	const [page, setPage] = React.useState(0)
-	const [rowsPerPage, setRowsPerPage] = React.useState(50)
+	const classes = useStyles(),
+	 [page, setPage] = React.useState(0),
+	 [rowsPerPage, setRowsPerPage] = React.useState(50),
 
-	const handleChangePage = (event, newPage) => {
-		setPage(newPage)
-	}
+	 handleChangePage = (event, newPage) => {
+			setPage(newPage)
+		},
 
-	const handleChangeRowsPerPage = event => {
-		setRowsPerPage(+event.target.value)
-		setPage(0)
-	}
+	 handleChangeRowsPerPage = event => {
+			setRowsPerPage(+event.target.value)
+			setPage(0)
+		}
 
 	return (
 		<>
@@ -116,7 +114,7 @@ function OrderHistory() {
 									<TableCell
 										key={ column.id }
 										align={ column.align }
-										style={ { minWidth: column.minWidth } }
+										style={ { minWidth : column.minWidth } }
 									>
 										{ column.label }
 									</TableCell>
@@ -133,7 +131,8 @@ function OrderHistory() {
 										key={ row.id }
 									>
 										{ columns.map(column => {
-											const value = row[column.id]
+											const value = row[ column.id ]
+
 											return (
 												<TableCell
 													key={ column.id }
@@ -167,7 +166,5 @@ function OrderHistory() {
 }
 
 export default withTracker(() => {
-	return {
-		orders: Orders.find().fetch(),
-	}
+	return { orders : Orders.find().fetch() }
 })(OrderHistory)
